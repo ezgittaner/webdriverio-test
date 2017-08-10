d94e4a9a161d6a7de683bffc45bb66a9cbc8b68c
Ezz <ezz@Recep-MacBook-Pro.local>
2017-08-10 12:00:23 +0300

    title

--- test/e2e/first.js
+++ test/e2e/first.js
@@ -4,7 +4,7 @@ const assert = require('assert')
 describe('First Test Group', () => {
   it('gets the title of MDN toppage', () => {
     const title = browser.url('https://www.ciceksepeti.com/').getTitle()
-    assert.equal(title, 'ciceksepeti')
+    assert.equal(title, 'Çiçek Sepeti - Türkiye\'nin En Büyük Çiçek Sitesi')
    
   })
 })
