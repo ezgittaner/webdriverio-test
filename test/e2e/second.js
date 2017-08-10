1e883f7b483ac5824cd3df5a7cb0cd65ac2d0214
Ezz <ezz@Recep-MacBook-Pro.local>
2017-08-10 15:00:02 +0300

    ezgitaner

---
+++ test/e2e/second.js
@@ -0,0 +1,9 @@
+'use strict'
+const assert = require('assert')
+
+describe('First Test Group', () => {
+  it('gets the title of MDN toppage', () => {
+    const title = browser.url('http://ezgitaner.com/').getTitle()
+    
+  })
+})
