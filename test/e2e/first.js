7fad9ed98dd031999bbf8322c3f7d803705fb781
Ezz <ezz@Recep-MacBook-Pro.local>
2017-08-10 14:41:42 +0300

    onuralp

--- test/e2e/first.js
+++ test/e2e/first.js
@@ -3,7 +3,7 @@ const assert = require('assert')
 
 describe('First Test Group', () => {
   it('gets the title of MDN toppage', () => {
-    const title = browser.url('https://www.ciceksepeti.com/').getTitle()
+    const title = browser.url('https://www.ciceksepeti.com/cicek').getTitle()
     assert.equal(title, 'Çiçek Sepeti - Türkiye\'nin En Büyük Çiçek Sitesi')
    
     console.log(browser.getText('.last-page'));
