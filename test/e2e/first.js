ddfe32cfcefa73992095532ae4c6c2b59e1f6173
Ezz <ezz@Recep-MacBook-Pro.local>
2017-08-10 11:50:57 +0300

    browser.wait del

--- test/e2e/first.js
+++ test/e2e/first.js
@@ -5,7 +5,6 @@ describe('First Test Group', () => {
   it('gets the title of MDN toppage', () => {
     const title = browser.url('https://developer.mozilla.org/en-US/').getTitle()
     assert.equal(title, 'Mozilla Developer Network')
-    // onuralp browser 5 saniye beklesin diye ekledi
-    browser.waitForVisible('#myElem', 5000);
+   
   })
 })
--- test/e2e/second.js
+++ test/e2e/second.js
@@ -5,6 +5,6 @@ describe('Second Test Group', () => {
   it('gets the title of GitHub toppage', () => {
     const title = browser.url('https://github.com/').getTitle()
     assert.equal(title, 'The world\'s leading software development platform · GitHub')
-    browser.waitForVisible('#myElem', 5000);
+    
   })
 })
