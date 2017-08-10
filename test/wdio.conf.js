5380a9c4f4624753c4e8ee57388bb85916853019
Ezz <ezz@Recep-MacBook-Pro.local>
2017-08-10 11:40:54 +0300

    chrome

--- test/wdio.conf.js
+++ test/wdio.conf.js
@@ -2,7 +2,7 @@
 
 exports.config = {
   capabilities: [
-    { browserName: 'firefox ' }
+    { browserName: 'chrome ' }
     // If you want to use other browsers,
     // you may need local Selenium standalone server.
   ],
